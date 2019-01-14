1c5c523b4cea79b6563b222dbd2f5555d6f2376c
Tim Keiner <keiner.tim@gmail.com>
2019-01-14 14:33:01 +0100

    let every verification fail

--- citurs-intro/step2-verify.sh
+++ citurs-intro/step2-verify.sh
@@ -11,5 +11,6 @@ JAVA_TEST=/home/scrapbook/tutorial/citrus-sample/src/test/java/org/citrus/sample
 XML_CONFIG=/home/scrapbook/tutorial/citrus-sample/src/test/resources/citrus-context.xml
 
 verifyContent $JAVA_TEST "private HttpClient todoClient" && \
-verifyContent $XML_CONFIG "<citrus-http:client id=\"todoClient\" request-url=\"http://todo-app.paas.consol.de\" />" && \
-echo "done"
+verifyContent $XML_CONFIG "<citrus-http:client id=\"todoClient\" request-url=\"http://todo-app.paas.consol.de\" />"
+
+echo "Not Finished Padawan"
