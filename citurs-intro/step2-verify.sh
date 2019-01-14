6169d59c5ef58e48ba94e80a0c21d1d281ef3de6
Tim Keiner <keiner.tim@gmail.com>
2019-01-14 14:38:29 +0100

    fixed verification of step2

--- citurs-intro/step2-verify.sh
+++ citurs-intro/step2-verify.sh
@@ -4,12 +4,14 @@ function verifyContent() {
     FILE=$1
     NEEDLE=$2
 
-    cat ${FILE} | grep NEEDLE > /dev/null
+    cat ${FILE} | grep "${NEEDLE}" > /dev/null
 }
 
 JAVA_TEST=/home/scrapbook/tutorial/citrus-sample/src/test/java/org/citrus/samples/TodoAppIT.java
 XML_CONFIG=/home/scrapbook/tutorial/citrus-sample/src/test/resources/citrus-context.xml
 
+#TEST1=/Users/timkeiner/Projects/consol/github/katacoda-scenarios/citurs-intro/intro.md
+#verifyContent $TEST1 "System X under test" && \
 verifyContent $JAVA_TEST "private HttpClient todoClient" && \
 verifyContent $XML_CONFIG "<citrus-http:client id=\"todoClient\" request-url=\"http://todo-app.paas.consol.de\" />" && \
 echo "done"
