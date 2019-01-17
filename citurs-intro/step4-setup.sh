ca697a9b715dbb53f72cbfb23f86bf873ff41682
Tim Keiner <keiner.tim@gmail.com>
2019-01-17 11:46:19 +0100

    add citrus vars to test

--- citurs-intro/step4-setup.sh
+++ citurs-intro/step4-setup.sh
@@ -3,29 +3,22 @@
 touch /home/scrapbook/tutorial/citrus-sample/src/test/java/org/citrus/samples/TodoAppLifecycleIT.java
 cat > /home/scrapbook/tutorial/citrus-sample/src/test/java/org/citrus/samples/TodoAppLifecycleIT.java << EOM
 package org.citrus.samples;
-
 import org.testng.annotations.Test;
-
 import com.consol.citrus.annotations.CitrusTest;
 import com.consol.citrus.dsl.testng.TestNGCitrusTestDesigner;
 import com.consol.citrus.http.client.HttpClient;
 import com.consol.citrus.message.MessageType;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-
 public class TodoAppLifecycleIT extends TestNGCitrusTestDesigner {
-
     @Autowired
     private HttpClient todoClient;
-
     @Test
     @CitrusTest
     public void testTodoLifecycle() {
         variable("todoId", "citrus:randomUUID()");
         variable("todoName", "citrus:concat('todo_', citrus:randomNumber(4))");
-        variable("todoDescription", "Description: ${todoName}");
-
+        variable("todoDescription", "Description: ...");
         http()
             .client(todoClient)
             .send()
@@ -36,20 +29,17 @@ public class TodoAppLifecycleIT extends TestNGCitrusTestDesigner {
                     "\"title\": \"${todoName}\", " +
                     "\"description\": \"${todoDescription}\", " +
                     "\"done\": false}");
-
         http()
             .client(todoClient)
             .receive()
             .response(HttpStatus.OK)
             .messageType(MessageType.PLAINTEXT)
-            .payload("${todoId}");
-
+            .payload("");
         http()
             .client(todoClient)
             .send()
             .get("/api/todo/${todoId}")
             .accept("application/json");
-
         http()
             .client(todoClient)
             .receive()
@@ -59,13 +49,11 @@ public class TodoAppLifecycleIT extends TestNGCitrusTestDesigner {
             .validate("$.title", "${todoName}")
             .validate("$.description", "${todoDescription}")
             .validate("$.done", false);
-
         http()
             .client(todoClient)
             .send()
             .delete("/api/todo/${todoId}")
             .accept("application/json");
-
         http()
             .client(todoClient)
             .receive()
